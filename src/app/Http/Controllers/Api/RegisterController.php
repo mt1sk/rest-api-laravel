8a3510c58f2fc62fd417ebf7bbe58d1ed7a7f397
mt1sk <mt1sk@mail.ru>
2019-09-17 17:19:55 +0300

    access_token passport_driver api_registration auth_through_laravel_passport.

---
+++ src/app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\StoreUserRequest;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['success'=>true, 'user'=>$user->toArray()], 201);
+    }
+}
